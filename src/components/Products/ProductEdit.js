5f1c42380dab062e54bc7e03da9240a8c27ba66c
fernandez <fernandoab@hotmail.com.ar>
2021-01-25 22:33:45 -0300

    add new product code moved to productedit file

---
+++ src/components/Products/ProductEdit.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react';
+import { useFetch } from '../../useFetch';
+import { useParams } from "react-router-dom";
+
+
+const ProductDetail = () => {
+
+  const { id } = useParams();
+
+  const { status, data, error } = useFetch("http://localhost:3004/products/" + id);
+
+  const product = data;
+
+  const [formData, setForm] = useState({userID: 1});
+
+  const myChangeHandler = (event) => {
+    let nam = event.target.name;
+    let val = event.target.value;
+    setForm({
+        ...formData,
+        [nam]: val
+    })
+  }
+
+  const saveProduct = e => {
+    e.preventDefault()
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(formData)
+    };
+    fetch('http://localhost:3004/products/', requestOptions)
+      .then(response => response.json())
+      .then(data => console.log(data.id));
+
+  }
+
+  return (
+    <form>
+      product detail
+      <div>
+        <label for="title">Nombre</label>
+        <input name="title" type="text" value={product.title} onChange={myChangeHandler} />
+      </div>
+      <div>
+        <label for="description">Descripcion</label>
+        <textarea name="description" type="text" value={product.description} onChange={myChangeHandler}/>
+      </div>
+      <div>
+        <label for="price">Precio</label>
+        <input name="price" type="text" value={product.price}  onChange={myChangeHandler}/>
+      </div>
+      <div>
+        <label for="type">Tipo</label>
+        <input name="type" type="text" value={product.type}  onChange={myChangeHandler}/>
+      </div>
+      <button onClick={saveProduct}>GUardar</button>
+    </form>
+  );
+};
+
+
+
+export default ProductDetail;
